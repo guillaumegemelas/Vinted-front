0eabf17b472e6ae02ae4224a31a508f030127085
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-14 16:08:49 +0100

    update2

---
+++ src/Components/CheckoutForm.js
@@ -0,0 +1,66 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import axios from "axios";
+
+const CheckoutForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  //state qui sert à savoir si le paiement a bien été effectué
+  const [completed, setCompleted] = useState(false);
+
+  //permettra de créer une requête vers stripe pour obtenir un token
+  const stripe = useStripe();
+
+  //permettra de récupérer les donnéesbancaires de l'utilisateur
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      //je fais passer isLoading à true
+      setIsLoading(true);
+      //je récupère le contenu de l'input de CardElement
+      const cardElement = elements.getElement(CardElement);
+
+      const stripeResponse = await stripe.createToken(cardElement, {
+        name: "id de l'acheteur",
+      });
+
+      const stripeToken = stripeResponse.token.id;
+      console.log(stripeToken);
+
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/payment",
+        {
+          stripeToken: stripeToken,
+        }
+      );
+      console.log(response.data);
+
+      // si réponse du backend, succeeded
+      if (response.data === "succeeded") {
+        //on modifie les states isLoading et Completed
+        setIsLoading(false);
+        setCompleted(true);
+      }
+
+      //on envoie les inforamtions à stripe
+    } catch (error) {
+      console.log(error.message, "erreur 🤒");
+    }
+  };
+
+  return (
+    <form className="cardElement" onSubmit={handleSubmit}>
+      <CardElement />
+      {completed ? (
+        <p>Paiement effectué</p>
+      ) : (
+        <button disabled={isLoading} type="submit">
+          Payer
+        </button>
+      )}
+    </form>
+  );
+};
+
+export default CheckoutForm;
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,12 +1,27 @@
 import { useLocation } from "react-router-dom";
 
+//import Stripe
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../Components/CheckoutForm";
+
 const Payment = () => {
   const location = useLocation();
   const { title, price } = location.state;
 
-  let sendPrice = "3,8";
-  let protectPrice = "0,4";
-  let totalPrice = (Number(price) + Number(3.8) + Number(0.4)).toFixed(2);
+  //
+  const stripePromise = loadStripe(
+    "pk_test_51MbMv9KWnFPSCRftX2iOsV3iYagReSPaf7v6CYoQeioi7Qeo2uCsaKa4AvsK35lloAaJcVxpGrYHhJZWhTIpgBaR00gw7Kn7Zb"
+  );
+  //
+
+  let sendPrice = ((20 / 100) * price).toFixed(2);
+  let protectPrice = ((10 / 100) * price).toFixed(2);
+  let totalPrice = (
+    Number(price) +
+    Number(sendPrice) +
+    Number(protectPrice)
+  ).toFixed(2);
 
   return (
     <div className="container12">
@@ -38,6 +53,12 @@ const Payment = () => {
             <span>{title}</span>. Vous allez payer <span>{totalPrice}€ </span>
             (frais de protection et frais de port inclus)
           </p>
+        </div>
+        <div className="payBut">
+          {/* Elements va devoir englober toute la logique de paiement */}
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
         </div>
       </div>
     </div>
